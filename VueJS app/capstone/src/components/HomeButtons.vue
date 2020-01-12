1be0120116b50d07ac3b439ca58365c81e97f9bd
KeganLavoy <kegan.lavoy@hotmail.com>
2020-01-11 22:26:35 -0600

    Initial setup of Vue.JS project

---
+++ VueJS app/capstone/src/components/HomeButtons.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Click Here</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "HomeButtons"
+}
+</script>
+
+
+<style scoped>
+
+
+
+</style>
---
+++ VueJS app/capstone/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="I changed the prop"/>
+    <HomeButtons />
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import HomeButtons from '@/components/HomeButtons.vue'
+
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    HomeButtons
+  }
+}
+</script>
