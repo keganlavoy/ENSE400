b33af0df949c5cda335e0ba61dc98f80ead6da0f
daris.lychuk <daris.lychuk@hotmail.com>
2020-01-12 19:49:15 -0600

    Created login page

---
+++ VueJS app/capstone/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login">
+   
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+.login {
+background-image: url(../assets/main-background.jpeg);
+background-size: cover;
+height: 100%;
+overflow: hidden;
+text-align: center;
+
+}
+
+</style>
