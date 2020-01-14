dac1084495bbae3c32eba960eb11908945e301dc
daris.lychuk <daris.lychuk@hotmail.com>
2020-01-13 21:58:03 -0600

    Updated CSS on input fields, text, and buttons on login page

--- VueJS app/capstone/src/views/Login.vue
+++ VueJS app/capstone/src/views/Login.vue
@@ -3,12 +3,12 @@
       
    <div class="top-buffer">
    </div>
-        <input type="text" name="email" v-model="input.email" placeholder="Email" />
+        <input type="text" id="emailInput" name="email" v-model="input.email" placeholder="Email" />
         <br>
         <br>
-        <input type="password" name="password" v-model="input.password" placeholder="Password" />
+        <input type="password" id="passInput" name="password" v-model="input.password" placeholder="Password" />
         <br>
-        <router-link to="/" id="forgot-pass">Forgot your password?</router-link>
+        <router-link to="/" id="forgot-pass">Forgot password?</router-link>
         <br>
         <br>
         <button type="button" id="login-button" @click="$router.push('/')">Login</button>
@@ -35,45 +35,71 @@ export default {
 
 <style>
 
-.login {
-background-image: url(../assets/main-background.jpeg);
-background-size: cover;
-height: 100%;
-overflow: hidden;
-text-align: center;
+#emailInput {
+  border-radius: 7px;
+  padding: 5px;
+  outline: none;
+  width: 20%;
+}
 
+#emailInput:focus {
+  border: 3px solid #555;
+}
+
+#passInput {
+  border-radius: 7px;
+  padding: 5px;
+  outline: none;
+  width: 20%;
+}
+
+#passInput:focus {
+  border: 3px solid #555;
+}
+
+.login {
+  background-image: url(../assets/main-background.jpeg);
+  background-size: cover;
+  height: 100%;
+  overflow: hidden;
+  text-align: center;
 }
 
 #login-button {
   background-color: #f1f1f1;
   color: black;
   font-size: 16px;
-  width: 15%;
+  width: 20%;
   cursor: pointer;
-  border-radius: 5px;
+  border-radius: 7px;
 }
 
 #login-button:hover {
-background-color: black;
-color: white;
+  background-color: black;
+  color: white;
 }
 
 #signUp-button {
   background-color: #f1f1f1;
   color: black;
   font-size: 16px;
-  width: 15%;
+  width: 20%;
   cursor: pointer;
-  border-radius: 5px;
+  border-radius: 7px;
 }
 
 #signUp-button:hover {
-background-color: black;
-color: white;
+  background-color: black;
+  color: white;
 }
 
 #forgot-pass {
-font-size: 70%;
+  font-size: 100%;
+  position: absolute;
+  left: 40%;
+}
 
+#forgot-pass:hover {
+  font-size: 120%;
 }
 </style>
