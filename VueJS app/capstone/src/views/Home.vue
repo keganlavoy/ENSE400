a7d5ace1ded526efc1aad5f59d581326cda5d278
KeganLavoy <kegan.lavoy@hotmail.com>
2020-02-03 19:28:08 -0600

    Added our about us section of the homepage

--- VueJS app/capstone/src/views/Home.vue
+++ VueJS app/capstone/src/views/Home.vue
@@ -63,11 +63,47 @@
     </div>
     
     <div class="CenterAboutUs">
-      <div class="center-text">
+      <div class="odd-paragraph">
+
+         <h2>What is our vision?</h2>
+           <ul class="aboutUsText">
+              <li>We want to create a place where people feel comfortable and confident that they are making the smartest decisions for them when it comes to their insurance.</li>
+              <br>
+              <li>We want to make finding the best deal as easy and fast as possible and provide direct comparisons between insurance providers so that you don't have to.</li>
+              <br>
+              <li>In adition, we envision creating a community that freely shares their own experiences and can further help others make the best choices for them.</li>
+           </ul>
         
 
-        
       </div>
+
+      <div class="even-paragraph">
+
+         <h2>What makes us different?</h2>
+
+          <ul class="aboutUsText">
+              <li>Instead of comparing only final prices, we also display which options that you selected are covered by each inusrance provider. 
+                  This allows you see exactly what you are getting in the packages offered by each provider, and not just a magic number.</li>
+                  <br>
+              <li>Another thing that sets us apart is our community. People being able to share their experiences and situations allows others to take 
+                  their advice and make great decisions of their own.</li> 
+           </ul>
+  
+      </div>
+
+      <div class="odd-paragraph">
+
+         <h2>What is our Motivation?</h2>
+
+          <ul class="aboutUsText">
+              <li>We have seen and heard enough people struggle with finding the right insurance plan for them. The countless hours of web searching and manual 
+                  comparisons between them are stressful and tiring. We are here to change that. We provide a centralized place where you can compare insurance providers stress free.</li>       
+           </ul>
+      
+      </div>
+
+
+
     </div>
   
   </div>
@@ -121,8 +157,15 @@ background-color: white;
 height: 100%;
 width: 100%;
 overflow: auto;
-
+-ms-overflow-style: none;
 }
+
+.homepage::-webkit-scrollbar {
+  display: none;
+}
+
+
+
 .quotes {
 
 background-size: cover;
@@ -170,20 +213,33 @@ overflow: hidden;
 
   text-align: center;
   font-size: 2.8vw;
-   font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 
 }
 
 p {
   font-size: 1vw;
   font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  text-align: left;
 }
 
 h2 {
  font-size: 1.4vw;
-  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+ font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 
 }
+
+
+
+
+ul.aboutUsText {
+  font-size: 1vw;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  text-align: left;
+  
+}
+
+
 .top-buffer {
 height: 20%;
 
@@ -233,7 +289,27 @@ overflow: hidden;
 }
 
 
+.CenterAboutUs {
 
+width: 60%;
+height: auto;
+margin-top: 1%;
+margin-left: 20%;
+border: 1px solid black;
+
+}
+
+.odd-paragraph {
+  margin-top: -1.9%;
+  background-color:rgb(246, 246, 246);
+  height: auto;
+}
+
+.even-paragraph {
+  margin-top: -1.9%;
+  background-color: white;
+  height: auto;
+}
 
 .InsurancePicture {
 
