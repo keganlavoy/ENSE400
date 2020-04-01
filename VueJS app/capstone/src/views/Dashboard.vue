1ede819757f14967b6f52c87c2ca3b66b332ec84
KeganLavoy <kegan.lavoy@hotmail.com>
2020-04-01 17:36:33 -0600

    added comments and fixed code readability for the dashboard page

--- VueJS app/capstone/src/views/Dashboard.vue
+++ VueJS app/capstone/src/views/Dashboard.vue
@@ -1,116 +1,132 @@
 <template>
 
 <div class="mainDivDashboard">
-  <div class="quotes">
+
+    <!--Div for first block on dashboard-->
+    <div class="quotes">
         
+        <!--Title for Dashboard-->
         <div class="title-quotes">
          <h1 class="Home-H1">Get quotes fast for the insurance you need!</h1>
         </div>
     
+        <!--Contains the text on the left side of the screen of first block on dashboard-->
         <div class="left-quotes">
-         <div class="left-text">
+          <div class="left-text">
             <h2 class="sub-title">1. Select</h2>
             <p class="paragraph-dashboard">Choose which insurance plan is the best fit for you, or even choose a package deal.</p>
             <h2 class="sub-title">2. Compare</h2>
             <p class="paragraph-dashboard">Choose the options you want, and compare quotes side by side to find the best option for you.</p>
-           <h2 class="sub-title">3. Share</h2>
+            <h2 class="sub-title">3. Share</h2>
             <p class="paragraph-dashboard">Feel free to share your experience with us, along with others.</p>
-        
-         </div>
-       </div>
+          </div>
+        </div>
   
-    <div class="right-quotes">
-      
-      <div class="InsurancePicture"></div>
-      <div class="ComparePicture"></div>
-      <QuoteButton msg="Get Quotes" @click.native="$router.push(`/HealthInsurance/${user_id}`)"/>
+
+        <!--Contains the two pictures and get quotes button on the right side of first block on dashboard-->
+        <div class="right-quotes">
+          <div class="InsurancePicture"></div>
+          <div class="ComparePicture"></div>
+          <QuoteButton msg="Get Quotes" @click.native="$router.push(`/HealthInsurance/${user_id}`)"/>
+        </div>
 
     </div>
-  </div>
 
+
+
+  <!--Div for second block on dashboard-->
   <div class="Community">
 
+    <!--Title for second block of dashboard-->
     <div class="title-community">
       <h1 class="Home-H1">Share your experiences to help others!</h1>
     </div>
     
+
+    <!--Holds the text on the left side of the screen of second block of dashboard-->
     <div class="left-community">
       <div class="left-text">
         <h2 class="sub-title">Community</h2>
         <p class="paragraph-dashboard">Become part of our community and help us grow and reach others.</p>
         <h2 class="sub-title">Experiences</h2>
         <p class="paragraph-dashboard">See what others have said about their experiences and add your own into the mix.</p>
-      
       </div>
     </div>
   
+
+    <!--Holds the picture and share experiences button on the right side of screen of the second block of dashboard-->
     <div class="right-community">
-      
       <div class="CommunityPicture"></div>
       <CommunityButton msg="Share your experiences" @click.native="$router.push(`/Forum/${user_id}`)"/>
-
     </div>
   </div>
 
-   <div class="AboutUs">
 
-    <div class="title-about">
-      <h1 class="Home-H1">About Us</h1>
-    </div>
+
+    <!--Div for third block of dashboard-->
+    <div class="AboutUs">
+
+      <!--Title for third block of dashboard-->
+      <div class="title-about">
+        <h1 class="Home-H1">About Us</h1>
+      </div>
     
-    <div class="CenterAboutUs">
-      <div class="odd-paragraph">
 
-         <h2 class="sub-title">What is our vision?</h2>
-           <ul class="aboutUsText">
+      <!--Holds all info displayed in the about us block-->
+      <div class="CenterAboutUs">
+        <div class="odd-paragraph">
+
+          <h2 class="sub-title">What is our vision?</h2>
+            <ul class="aboutUsText">
               <li>We want to create a place where people feel comfortable and confident that they are making the smartest decisions for them when it comes to their insurance.</li>
               <br>
               <li>We want to make finding the best deal as easy and fast as possible and provide direct comparisons between insurance providers so that you don't have to.</li>
               <br>
               <li>In addition, we envision creating a community that freely shares their own experiences and can further help others make the best choices for them.</li>
-           </ul>      
+            </ul>      
+        </div>
 
-      </div>
 
-      <div class="even-paragraph">
+        <div class="even-paragraph">
 
-         <h2 class="sub-title">What makes us different?</h2>
+          <h2 class="sub-title">What makes us different?</h2>
 
-          <ul class="aboutUsText">
+            <ul class="aboutUsText">
               <li>Instead of comparing only final prices, we also display which options that you selected are covered by each inusrance provider. 
-                  This allows you see exactly what you are getting in the packages offered by each provider, and not just a magic number.</li>
-                  <br>
+                  This allows you see exactly what you are getting in the packages offered by each provider, and not just a magic number.</li><br>
               <li>Another thing that sets us apart is our community. People being able to share their experiences and situations allows others to take 
                   their advice and make great decisions of their own.</li> 
-           </ul>
-  
-      </div>
+            </ul>
+        </div>
 
-      <div class="odd-paragraph">
 
-         <h2 class="sub-title">What is our motivation?</h2>
+        <div class="odd-paragraph">
 
-          <ul class="aboutUsText">
+          <h2 class="sub-title">What is our motivation?</h2>
+
+            <ul class="aboutUsText">
               <li>We have seen and heard enough people struggle with finding the right insurance plan for them. The countless hours of web searching and manual 
                   comparisons between them are stressful and tiring. We are here to change that. We provide a centralized place where you can compare insurance providers stress free.</li>       
-           </ul>      
-      </div>
-    </div>  
-  </div>
-<div class="dashboardFooter">
-</div>
+            </ul>      
+        </div>
+      </div>  
+    </div>
+    
+    <!--adds space at bottom of page for a footer-->
+    <div class="dashboardFooter"></div>
 
-<div>
-  <ProfileButton class="ProfileButton" @click.native="$router.push(`/ProfilePage/${user_id}`)"/>
-</div>
+    <!--creates profile button in top right corner. Uses absolute positioning so it is placed in top corner.-->
+    <div>
+      <ProfileButton class="ProfileButton" @click.native="$router.push(`/ProfilePage/${user_id}`)"/>
+    </div>
 </div>
 
 
 </template>
 
 <script>
-// @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
+
+//import all comonents needed for buttons
 import QuoteButton from '@/components/getQuotesButton.vue'
 import CommunityButton from '@/components/CommunityButton.vue'
 import ProfileButton from '@/components/ProfileButton.vue'
@@ -135,7 +151,7 @@ export default {
   },
 
   created() {
-
+    //sets the user id to the user id gotten from login
     this.user_id = this.$route.params.user_id;
    
   }
@@ -281,6 +297,8 @@ overflow: hidden;
   background-size: cover;  
 }
 
+
+/*All media tags below here adjust the size and position of the pictures based on the size of the screen of the user*/
 @media screen and (max-width: 1365px){
   .InsurancePicture {
     width: 70%;
