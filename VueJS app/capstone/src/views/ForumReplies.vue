f890aa343e33da603de2fa3e691704d2249f6704
daris.lychuk <daris.lychuk@hotmail.com>
2020-03-31 22:51:11 -0600

    Finalized CSS view forum replies page

--- VueJS app/capstone/src/views/ForumReplies.vue
+++ VueJS app/capstone/src/views/ForumReplies.vue
@@ -3,7 +3,6 @@
 <div class="mainDivForumReply">
 
   <div class="CommunityForumReply">
-
     
     <div class="center-community-ForumReplies">
        <button type="button" class="forumRepliesButton" @click="$router.push(`/Forum/${user_id}`)">Back to home</button>
@@ -21,18 +20,15 @@
         <div id="originalPost"><h3 class="forumAddReply-body">{{post[0].post_body}}</h3></div>
     </div>
 
-
     <div class="center-community-ForumReplies" id="center-community-tabs-ForumReplies">
       <div id="replyHeader"><h2 class="forumAddReply-H2">Replies</h2></div>
 
     <div class="border">
       <div v-bind:key="reply.reply_id" v-bind:index="index" v-for="(reply, index) in replies">
-        <div id="filler"><h2 class="replyNumbering">Reply #: {{index + 1}}</h2></div>
+        <div id="filler"><h2 class="replyNumbering">Reply: #{{index + 1}}</h2></div>
         <div id="replyHeading"><h3 class="forumReply-Titles">Reply title: {{reply.reply_title}}</h3></div>
         <div id="replyNameHeading"><h3 class="forumReply-Titles">Reply posted by: {{reply.username}}</h3></div>
         <div id="originalPost"><h3 class="forumReply-body">{{reply.reply_body}}</h3></div>
-
-
       </div>
     </div>
 
@@ -110,77 +106,67 @@ export default {
 
   }
 
-
-
 }
 </script>
 
 
 <style>
 
+.forumAddReply-H2, .replyNumbering {
+  font-size: 22px;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
 #replyHeading {
   float: left;
   margin-left: 10%;
   width: 35%;
-  text-decoration: underline;
- 
+  text-decoration: underline; 
 }
 
 #replyHeader {
-    width: 100%;
-    height: 5.5vh;
-    margin-top: -1.25%;
-    margin-bottom: 0%;
-    background-color:rgb(30, 35, 39);
-    border-radius: 1vh 1vh 0vh 0vh;
-    border-bottom: 3px solid white;
-    
-
+  width: 100%;
+  height: 5.5vh;
+  margin-top: -19px;
+  margin-bottom: 0%;
+  background-color:rgb(30, 35, 39);
+  border-radius: 3px 3px 0px 0px;
+  border-bottom: 3px solid white;
 }
 
 #replyNameHeading {
-   float: left;
+  float: left;
   width: 35%;
   margin-left: 5%;
-  text-decoration: underline;
-  
+  text-decoration: underline;  
 }
-
-
 
 #topics {
   margin-right: 20%;
 }
 
 .forumReply-Titles {
-
  color: black;
- font-size: 2vw;
+ font-size: 22px;
  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-
 }
 
 .forumReply-body {
- font-size: 0.8vw;
- font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
- color: black;
- text-align: left;
- margin-left: 1%;
- margin-bottom: 2vh;
+  font-size: 18px;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: black;
+  text-align: left;
+  margin-left: 1%;
+  margin-bottom: 2vh;
 }
 
 #filler {
   float: left;
   color: white;
   width: 100%;
-  height: 5.5vh;
+  height: 50px;
   background-color: rgb(30, 35, 39);
 }
-
-
-
-
-
 
 .forumRepliesButton {
   float: left;
@@ -197,83 +183,50 @@ export default {
 }
 
 .CommunityForumReply {
-background-size: cover;
-background-color: rgb(243, 243, 243);
-height: 85%;
-width: 100%;
-overflow: hidden;
-
-
+  background-size: cover;
+  background-color: rgb(243, 243, 243);
+  height: 85%;
+  width: 100%;
+  overflow: hidden;
 }
 
 .ForumReplies-h1 {
-  float: left;
   text-align: center;
-  font-size: 2.8vw;
+  font-size: 28px;
+  color: white;
   font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   margin-top: 0%;
-  margin-left: 29%;
+  margin-right: 10%;
 }
-
-.ForumReplies-p {
-  font-size: 1vw;
-  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  text-align: left;
-}
-
-.ForumReplies-h2 {
- color: white;
- font-size: 1.4vw;
- font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-
-}
-
-.ForumReplies-h3 {
- font-size: 1.2vw;
- font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
- color: black;
- text-align: left;
- 
-}
-
-
-
 
 .center-community-ForumReplies {
-
-float: left;
-width: 100%;
-height: 9vh;
-margin-top: 3%;
-background-color: rgb(29, 134, 219);
-border-radius: 1vh 1vh 0vh 0vh;
-
+  float: left;
+  width: 100%;
+  height: 60px;
+  margin-top: 3%;
+  background-color: rgb(29, 134, 219);
+  border-radius: 8px 8px 0px 0px;
 }
 
 #center-community-subheader-ForumReplies {
-
-height: 2vh;;
-margin-top: 0%;
-background-color: rgb(30, 35, 39);
-border-radius: 0vh;
-
+  height: 4vh;
+  margin-top: 0%;
+  background-color: rgb(30, 35, 39);
+  border-radius: 0px;
 }
-/* add v-for to this css */
-#center-community-tabs-ForumReplies {
-width: 99.6%;
-height: 25%;
-margin-top: 0.5%;
-background-color: white;
-border: 3px solid rgb(30, 35, 39);
-border-radius: 1vh;
-margin-top: 2%;
 
+#center-community-tabs-ForumReplies {
+  width: 99.6%;
+  height: 25%;
+  margin-top: 0.5%;
+  background-color: white;
+  border: 3px solid rgb(30, 35, 39);
+  border-radius: 8px;
+  margin-top: 2%;
 }
 
 .invis {
   visibility: hidden;
-
 }
-
 
 </style>
