345812aeea2a24a8d53197ad4f7a93aefb2e1c47
KeganLavoy <kegan.lavoy@hotmail.com>
2020-04-01 22:42:47 -0600

    added some comments to profile page

--- VueJS app/capstone/src/views/ProfilePage.vue
+++ VueJS app/capstone/src/views/ProfilePage.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="mainDivProfile">
 
+
+    <!--Creates blue side bars and space at the top of the screen-->
     <div class="left-blue"></div>
     <div class="right-blue"></div>
-      
-   <div class="top-buffer">
-   </div>
+    <div class="top-buffer"></div>
 
-    <form>
+
+    <!--These are the input boxes that are used to insert, save, and display the user's info if they choose to enter it in.-->
         <input type="text" class="input" name="firstName" v-model="input.firstName" placeholder="First Name" />
         
         <input type="text" class="input" name="lastName" v-model="input.lastName" placeholder="Last Name" />
@@ -17,9 +18,9 @@
             <input type="text" class="input" id="DOBday" name="DOBday" v-model="input.DOBday" placeholder="Day" />
             <input type="text" class="input" id="DOByear" name="DOByear" v-model="input.DOByear" placeholder="Year" />
             <select name="gender" class="dropdown" v-model="input.gender">
-             <option value="null" selected disabled hidden>Gender</option>
-             <option value="male">Male</option>
-             <option value="female">Female</option>
+                <option value="null" selected disabled hidden>Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
             </select>
         </h4>
         
@@ -37,16 +38,18 @@
              <option value="Not married">Not married</option>
              <option value="married">Married</option>
              <option value="common Law">Common Law</option>
-         </select>
-        <input type="text" class="input" id="children" name="children" v-model="input.children" placeholder="How many children?" />
+        </select>
+        <input type="number" step="1" class="input" id="children" name="children" v-model="input.children" placeholder="How many children?" />
         <br><br>
+
+        <!--When this save button is clicked it will execute the function that saves the user entered data into the database-->
         <input type="submit" value="Save" class="button" @click="updateUser(user_id, input.firstName, input.lastName, input.DOBmonth, input.DOBday, input.DOByear, input.gender,
                                                                 input.homeAddress, input.city, input.province, input.postalCode, input.email, input.phoneNum,
-                                                                input.maritalStatus, input.children,)">
-    </form>
-        <br><br>
-        <button type="button" class="button" @click="$router.push(`/Dashboard/${user_id}`)">Back to home</button>
-    </div>
+                                                                input.maritalStatus, input.children)">
+    
+    <br><br>
+    <button type="button" class="button" @click="$router.push(`/Dashboard/${user_id}`)">Back to home</button>
+  </div>
 </template>
 
 <script>
@@ -60,7 +63,6 @@ export default {
         data() {
             return {
             
-                responses: [],
 
                 input: {
                     firstName: "",
@@ -87,7 +89,8 @@ export default {
 
     methods: {
 
-      
+        /*This is the function that updates all of the users information when they click the save button for the form. It takes the contents of every input box from the 
+          form and saves that info directly into the database.*/
         updateUser(user_id, firstName, lastName, DOBmonth, DOBday, DOByear, gender, homeAddress, city, province, postalCode, email, phoneNum, maritalStatus, children) {
 
         axios.post(`http://162.253.11.179:3000/updateUser/${user_id}/${firstName}/${lastName}/${DOBmonth}/${DOBday}/${DOByear}/${gender}/${homeAddress}/${city}/${province}/${postalCode}/${email}/${phoneNum}/${maritalStatus}/${children}`)
@@ -101,6 +104,8 @@ export default {
     },
 
     created() {
+        /*Whenever this page is loaded, it uses the user id passed from the route parameters from previous pages to query the database with that id 
+          and fill in the input boxes with that users information entered previously, if they have any.*/
 
         this.user_id = this.$route.params.user_id;
         var id = this.user_id;
