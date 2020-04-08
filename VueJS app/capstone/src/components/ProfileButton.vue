45e5f7acfb727f6cb08f3a0203a2642e5a79e5c4
daris.lychuk <daris.lychuk@hotmail.com>
2020-04-07 19:08:51 -0600

    Added "profile" signifier on profile button

--- VueJS app/capstone/src/components/ProfileButton.vue
+++ VueJS app/capstone/src/components/ProfileButton.vue
@@ -1,7 +1,7 @@
 <template>
     
 <div class="ProfileButton">
-
+  <h4 class="profileH4">Profile</h4>
 </div>
 
 </template>
@@ -31,10 +31,42 @@ export default {
   z-index: 1;
   width: 6%;
   height: 12%;
-  background-image: url(../assets/ProfileIcon.png), url(../assets/bannerLogoBG.png);
+  background-image: url(../assets/ProfileIcon.png);
   background-size: cover;
   border-left: 2px solid black;
   cursor: pointer;
 }
 
+.ProfileUser {
+  border: 2px solid black;
+}
+
+.profileH4 {
+  float: right;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 16px;
+  margin-top: 8vh;
+  margin-right: 1vw;
+}
+
+@media screen and (max-width: 1355px){
+  .profileH4 {
+    font-size: 14px;
+    margin-right: 0.5vw;
+  }
+}
+
+@media screen and (min-width: 1440px){
+  .profileH4 {
+    font-size: 18px;
+    margin-right: 0.5vw;
+  }
+}
+
+@media screen and (min-width: 1920px){
+  .profileH4 {
+    font-size: 20px;
+    margin-right: 1vw;
+  }
+}
 </style>
